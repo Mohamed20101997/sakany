00de399ea02b2054d1518dafe52ecdf4a5d6084f
Mohamed Adel <31623567+Mohamed20101997@users.noreply.github.com>
2022-03-03 14:34:56 +0200

    visible rouet front

--- routes/web.php
+++ routes/web.php
@@ -3,22 +3,22 @@
 use Illuminate\Support\Facades\Route;
 
 
-//Route::get('/','HomeController@index')->name('home');
-//Route::get('/details','HomeController@details')->name('details');
-//
-//
-//
-//Route::get('/logout', 'AuthController@logout')->name('front.logout');
-//
-//
-//
-//Route::group(['middleware' => 'guestfront'], function () {
-//
-//Route::get('/login','AuthController@login')->name('front.login');
-//
-//Route::post('/login','AuthController@login_post')->name('front.login_post');
-//
-//Route::get('/register','AuthController@register')->name('front.register');
+Route::get('/','HomeController@index')->name('home');
+Route::get('/details','HomeController@details')->name('details');
 
 
-//});
+
+Route::get('/logout', 'AuthController@logout')->name('front.logout');
+
+
+
+Route::group(['middleware' => 'guestfront'], function () {
+
+Route::get('/login','AuthController@login')->name('front.login');
+
+Route::post('/login','AuthController@login_post')->name('front.login_post');
+
+Route::get('/register','AuthController@register')->name('front.register');
+
+
+});
